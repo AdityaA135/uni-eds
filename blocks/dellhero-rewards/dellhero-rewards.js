d87409b836257acc41b611dfa41882930d11e96f
AdityaA135 <adityaagarwal664@gmail.com>
2026-06-04 15:59:56 +0530

    model debug

--- blocks/dellhero-rewards/dellhero-rewards.js
+++ blocks/dellhero-rewards/dellhero-rewards.js
@@ -1,8 +1,8 @@
 export default function decorate(block) {
-  console.log("aditya");
+  console.log("aditya 0");
   const row = block.children[0];
 
-  console.log("aditya");
+  console.log("aditya 1");
   if (!row) return;
 
   const contentDiv = row.children[0];
@@ -10,6 +10,7 @@ export default function decorate(block) {
 
   if (!contentDiv || !imageDiv) return;
 
+console.log("aditya 2");
   // Content extraction
   const eyebrow = contentDiv.querySelector('p:nth-of-type(1)');
   const heading = contentDiv.querySelector('h2');
@@ -21,11 +22,12 @@ export default function decorate(block) {
   const primaryLink = links[0];
   const secondaryLink = links[1];
 
+console.log("aditya 3");
   // Image extraction
   const picture = imageDiv.querySelector('picture');
 
   // Build final structure
-   console.log("aditya");
+   console.log("aditya 4");
   block.innerHTML = `
     <div>
       <div>
@@ -59,4 +61,5 @@ export default function decorate(block) {
       </div>
     </div>
   `;
+  console.log("aditya 5");
 }
